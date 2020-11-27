8636acd52879b0e7441a88995992516241687270
nicoalimin <nicoalimin@yahoo.com>
2020-11-26 18:28:16 -0800

    Add logo and layout changes

--- src/components/board.js
+++ src/components/board.js
@@ -1,6 +1,7 @@
-import { Avatar, Box, Grid, GridList, GridListTile, List, ListItem, ListSubheader, TextField } from '@material-ui/core'
+import { Avatar, Box, Button, Grid, GridList, GridListTile, List, ListItem, ListSubheader, TextField } from '@material-ui/core'
 import React from 'react'
 import Draggable from 'react-draggable'
+import logo from '../static/logo.jpeg'
 
 const playerGen = (name) => {
   return {
@@ -14,7 +15,7 @@ const players = [playerGen("p1"), playerGen("p2"), playerGen("p3"), playerGen("p
 
 const initialState = {
   size: 8,
-  players: [],
+  players: players,
   boxes: [],
   addPlayerName: ""
 }
@@ -91,18 +92,34 @@ export default class Board extends React.Component {
       )
     })
 
+    const handleAddPlayer = () => {
+      if (this.state.addPlayerName === "") return
+      if (!!this.state.players.find(p => p.name === this.state.addPlayerName)) return
+      const player = {
+        name: this.state.addPlayerName,
+        initials: this.state.addPlayerName.substring(0, 2),
+        color: `#${Math.floor(Math.random()*16777215).toString(16)}`,
+        points: 0
+      }
+      this.setState({
+        ...this.state,
+        players: [...this.state.players, player].sort((a, b) => (a.points - b.points)),
+        addPlayerName: initialState.addPlayerName
+      })
+    }
+
     return (
       <>
         <Grid container className="ular-mabok">
-          <Grid item xs={8}>
-            <Grid item>
-              <Grid item xs={12}>
+          <Grid item xs={9}>
+            <Grid container item>
+              <Grid item xs={10} className="top-bar">
+                <Avatar src={logo} className="ular-mabok-logo" />
                 <TextField
-                  id="ular-mabok-input"
+                  className="ular-mabok-input"
                   label="Add player"
                   placeholder="Insert name here..."
                   value={this.state.addPlayerName}
-                  style={{ margin: 8 }}
                   margin="normal"
                   InputLabelProps={{
                     shrink: true,
@@ -111,46 +128,42 @@ export default class Board extends React.Component {
                   onChange={(e) => {
                     this.setState({ ...this.state, addPlayerName: e.target.value })
                   }}
-                  onKeyDown={(e) => {
-                    if (e.key === 'Enter') {
-                      if (this.state.addPlayerName === "") return
-                      if (!!this.state.players.find(p => p.name === this.state.addPlayerName)) return
-                      const player = {
-                        name: this.state.addPlayerName,
-                        initials: this.state.addPlayerName.substring(0, 2),
-                        color: `#${Math.floor(Math.random()*16777215).toString(16)}`,
-                        points: 0
-                      }
-                      this.setState({
-                        ...this.state,
-                        players: [...this.state.players, player].sort((a, b) => (a.points - b.points)),
-                        addPlayerName: initialState.addPlayerName
-                      })
-                    }
-                  }}
+                  onKeyDown={(e) => { if (e.key === 'Enter') handleAddPlayer() }}
                 />
+              </Grid>
+              <Grid item xs={2} className="ular-mabok-button-container">
+                <Button variant="contained" color="primary" className="ular-mabok-button" onClick={() => handleAddPlayer()}>
+                  Add Player
+                </Button>
               </Grid>
             </Grid>
             <Grid item>
-              <GridList cols={8} className="ular-mabok-board">
+              <GridList cols={8} className="ular-mabok-board" style={{ margin: "auto" }}>
                 {boxes}
                 {peons}
               </GridList>
             </Grid>
           </Grid>
-          <Grid item xs={2}>
-            Dice
-          </Grid>
-          <Grid item xs={2}>
-            <List
-              subheader={
-                <ListSubheader>
-                  Leaderboard
-                </ListSubheader>
-              }
-            >
-              {listItems}
-            </List>
+          <Grid item container xs={3} style={{ paddingLeft: "10px" }}>
+            <Grid item container xs={12} className="dice-roll">
+              <Grid className="title" xs={12}>
+                Roll Me!
+              </Grid>
+              <Grid className="title" xs={12}>
+                Roll Me!
+              </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <List
+                subheader={
+                  <ListSubheader>
+                    Leaderboard
+                  </ListSubheader>
+                }
+              >
+                {listItems}
+              </List>
+            </Grid>
           </Grid>
         </Grid>
       </>
